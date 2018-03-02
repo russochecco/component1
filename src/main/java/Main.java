cba8fb9231ef85b57ff061f86a8da7b340d8fa67
Francesco Russo <francesco_russo@apple.com>
2018-03-02 12:46:12 +0000

    RP-5567: Removed person using shared VO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
+import org.frusso.sharedmodule1.vo.PersonVO;
+
 public final class Main {
 
     public static void main(String... args) {
 
-        final Person person = new Person();
+        final PersonVO personVO = new PersonVO("Francesco", "Russo", 44);
+        System.out.println(String.format("New %s", personVO));
 
-        person.setName("Francesco");
-        person.setSurname("Russo");
-        person.setAge(44);
-
-        System.out.println(String.format("New person %s", person));
-
+        System.exit(0);
     }
 }
