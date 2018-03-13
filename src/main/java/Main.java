6e7a6f317592c3ef86c3903dc57b52d5e0a2d36b
Francesco Russo <francesco_russo@apple.com>
2018-03-13 17:50:39 +0000

    Modified main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,7 @@ public final class Main {
     public static void main(String... args) {
 
         final PersonVO personVO = new PersonVO("Francesco", "Russo", 44);
-
         System.out.println(String.format("New %s", personVO));
-
         System.exit(0);
     }
 }
